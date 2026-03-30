f4061f0df468f2a38f0263dc8b01340db114bba2
Roman Kondrashov <nr.kondrashov@gmail.com>
2026-03-30 21:44:18 +0300

    Мелкая правка скрипта добавления чата.

--- .local/commands/add-chat.sh
+++ .local/commands/add-chat.sh
@@ -3,3 +3,4 @@
 # Добавление нового чата
 
 docker compose exec parser php scripts/add-chat.php # https://t.me/mezenrealism
+# docker compose exec parser php scripts/add-chat.php https://t.me/svjazki
