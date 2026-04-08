c9f3c7f36b0e05c7e05e1f0b94a49fe487e35785
Roman Kondrashov <nr.kondrashov@gmail.com>
2026-04-09 00:19:03 +0300

    Поправил скрипт синхронизации.

--- .local/commands/sync.sh
+++ .local/commands/sync.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# Синхронизация сообщений в чатах (обновление, загрузка новых)
+# Синхронизация сообщений в чатах
 
-# По умолчанию: новые сообщения, без лимита, все чаты
+# По умолчанию: все чаты, сообщения с лимитом по-умолчанию
 #docker compose exec parser php -d memory_limit=1024M scripts/sync.php
 
-# Загрузить старые сообщения для конкретного чата, лимит 5000
-docker compose exec parser php -d memory_limit=1024M scripts/sync.php --chat=-1001432413295
+# Конкретный чат, сообщения с лимитом по-умолчанию
+#docker compose exec parser php -d memory_limit=1024M scripts/sync.php --chat=-1001432413295
+
+# Конкретный чат, сообщения с лимитом 10000
+docker compose exec parser php -d memory_limit=1024M scripts/sync.php --chat=-1001432413295 --limit=10000
